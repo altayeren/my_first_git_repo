e93f127ae19d045c48de56997cc789ea09229cf5
altayeren <altayeren@gmail.com>
2022-08-03 22:49:25 +0300

    Fourth Commit

--- src/MyGitClass.java
+++ src/MyGitClass.java
@@ -2,6 +2,7 @@ public class MyGitClass {
     public static void main(String[] args) {
         System.out.println("I am learning Github!!!");
         System.out.println("Version 1");
+        System.out.println("I am adding one more..");
 
     }
 }
